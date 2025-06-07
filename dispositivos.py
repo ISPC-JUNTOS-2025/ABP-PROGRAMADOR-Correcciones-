4de0dd72c2161488ec962cdffcbe1f3c071dcf30
Dana Valverde <danakvalverde@gmail.com>
2025-06-07 13:29:03 -0300

    se creo un nuevo archivo para la gestion de las automatizacion, con el encendido y el apagado de la misma, se definio el menu de gestion de esta y se hicieron las respectivas validaciones

--- dispositivos.py
+++ dispositivos.py
@@ -1,11 +1,22 @@
 from datetime import datetime
 
-lista_de_dispositivos = []
-
-lista_de_automatizaciones = []
+lista_de_dispositivos = [{
+        "Id": 1,
+        "Tipo Dispositivo": "luces",
+        "Nombre Dispositivo": "luces" ,
+        "Estado Dispositivo": False, 
+        "Fecha de Creacion": datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    }, 
+    {
+        "Id": 2,
+        "Tipo Dispositivo": "electrodomestico",
+        "Nombre Dispositivo": "lavarropas",
+        "Estado Dispositivo": False, 
+        "Fecha de Creacion": datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    }
+    ]
 
 id_dispositivo = 1
-
 
 def crear_dispositivo(tipo_dispositivo, nombre_dispositivo):
     global id_dispositivo
@@ -42,8 +53,9 @@ def listar_dispositivos():
     for dispositivo in lista_de_dispositivos:
         print("Tipo Dispositivo:", dispositivo["Tipo Dispositivo"])
         print("Nombre Dispositivo:", dispositivo["Nombre Dispositivo"])
-        print("Estado:", "Encendido" if dispositivo["Estado"] else "Apagado")
+        print("Estado:", "Encendido" if dispositivo["Estado Dispositivo"] else "Apagado")
         print("-" * 40)  
+    return dispositivo    
 
 
 def eliminar_dispositivo_por_nombre(nombre_dispositivo):
@@ -54,16 +66,9 @@ def eliminar_dispositivo_por_nombre(nombre_dispositivo):
 
     return print(f"No se encontro el dispositivo: {nombre_dispositivo}")
 
+def devolver_lista_de_dispositivos():
+    return lista_de_dispositivos
 
-def automatizacion_encender_luces():
-    print("movimiento detectado en entrada principal")
 
-    for dispositivo in lista_de_dispositivos:
-        if dispositivo["Nombre Dispositivo"] == "luces".lower() and dispositivo["Estado Dispositivo"] == False:
-            dispositivo["Estado Dispositivo"] = True
-            return print("luces entrada principal encendidas")
-            
-
-    return print("No se han detectado luces a encender")    
 
 
