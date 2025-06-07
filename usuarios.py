cc2cc6edcec17b6bc74c71d25eb4b4169926e894
Juan Cruz Avila <juancurzavila@outlook.com>
2025-06-06 22:10:27 -0300

    se realizo la funcion para modificar el rol de usuario

--- usuarios.py
+++ usuarios.py
@@ -64,3 +64,22 @@
 #-------------------------------------------------------------------
 
 #Asignacion de roles(JuanX) - 20 Lineas
+
+def modificar_rol_usuario(usuarios, nombre_usuario, nuevo_rol):
+    if nombre_usuario in usuarios:
+        if nuevo_rol in ['admin', 'estandar']:
+            usuarios[nombre_usuario]['rol'] = nuevo_rol
+            print(f"Rol de {nombre_usuario} actualizado a {nuevo_rol}.")
+        else:
+            print("Rol inválido. Use 'admin' o 'estandar'.")
+    else:
+        print("Usuario no encontrado.")
+
+usuarios = {
+    'administrador': {'rol': 'admin'},
+    'invitado1': {'rol': 'estandar'},
+    'invitado2': {'rol': 'estandar'}
+}
+
+modificar_rol_usuario(usuarios, 'invitado1', 'admin')
+modificar_rol_usuario(usuarios, 'administrador', 'estandar')
